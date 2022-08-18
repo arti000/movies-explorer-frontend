7762976aed15b2ef697f433dc11dae4a0864d0df
Artem Kireev <al-mysri@yandex.ru>
2022-08-18 20:59:37 +0300

    fix: underline

--- src/components/Navigation/components/Authorized/Menu/Menu.js
+++ src/components/Navigation/components/Authorized/Menu/Menu.js
@@ -7,41 +7,47 @@ function Menu({ onClick }) {
     <section className='menu'>
       <nav className='menu__nav-menu'>
         <button className='menu__close-icon' onClick={onClick}></button>
-        <div className='menu__nav-list'>
-          <NavLink
-            to='/'
-            className={({ isActive }) =>
-              isActive
-                ? 'menu__link menu__link_active'
-                : 'menu__link'
-            }
-            onClick={onClick}
-          >
-            Главная
-          </NavLink>
-          <NavLink
-            to='/movies'
-            className={({ isActive }) =>
-              isActive
-                ? 'menu__link menu__link_active'
-                : 'menu__link'
-            }
-            onClick={onClick}
-          >
-            Фильмы
-          </NavLink>
-          <NavLink
-            to='/saved-movies'
-            className={({ isActive }) =>
-              isActive
-                ? 'menu__link menu__link_active'
-                : 'menu__link'
-            }
-            onClick={onClick}
-          >
-            Сохранённые фильмы
-          </NavLink>
-        </div>
+        <ul className='menu__nav-list'>
+          <li className='menu__nav-list-item'>
+            <NavLink
+              to='/'
+              className={({ isActive }) =>
+                isActive
+                  ? 'menu__link menu__link_active'
+                  : 'menu__link'
+              }
+              onClick={onClick}
+            >
+              Главная
+            </NavLink>
+          </li>
+          <li className='menu__nav-list-item'>
+            <NavLink
+              to='/movies'
+              className={({ isActive }) =>
+                isActive
+                  ? 'menu__link menu__link_active'
+                  : 'menu__link'
+              }
+              onClick={onClick}
+            >
+              Фильмы
+            </NavLink>
+          </li>
+          <li className='menu__nav-list-item'>
+            <NavLink
+              to='/saved-movies'
+              className={({ isActive }) =>
+                isActive
+                  ? 'menu__link menu__link_active'
+                  : 'menu__link'
+              }
+              onClick={onClick}
+            >
+              Сохранённые фильмы
+            </NavLink>
+          </li>
+        </ul>
         <NavLink
           to='/profile'
           className='menu__link menu__link_profile'>
